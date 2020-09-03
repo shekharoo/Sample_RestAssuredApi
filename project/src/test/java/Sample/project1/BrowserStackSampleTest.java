276243115af8cd4c1153d95857c47f28d8400dd2
shekharoo <shekuemail@gmail.com>
2020-09-03 20:02:26 +0530

    1st Commit

---
+++ project/src/test/java/Sample/project1/BrowserStackSampleTest.java
@@ -0,0 +1,35 @@
+package Sample.project1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
+
+public class BrowserStackSampleTest {
+  public static final String USERNAME = "shekharanand1";
+  public static final String AUTOMATE_KEY = "nJHsTsrcPzP32eYXsw8p";
+  public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+  public static void main(String[] args) throws Exception {
+    DesiredCapabilities caps = new DesiredCapabilities();
+    
+    caps.setCapability("os", "Windows");
+    caps.setCapability("os_version", "10");
+    caps.setCapability("browser", "Chrome");
+    caps.setCapability("browser_version", "81");
+    
+    caps.setCapability("name", "shekharanand1's First Test");
+    
+    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+    driver.get("http://www.google.com");
+    WebElement element = driver.findElement(By.name("q"));
+
+    element.sendKeys("Shekhar Anand");
+    element.submit();
+
+    System.out.println(driver.getTitle());
+    driver.quit();
+  }
+}
